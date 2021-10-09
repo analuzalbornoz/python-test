355a3660c41243ff1fd8830ca8d2416451fd9e52
analuzalbornoz <85367404+analuzalbornoz@users.noreply.github.com>
2021-10-09 11:47:39 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,6 @@
+setup(
+    name='prueba',
+    version='1',
+    author='Analuz Albornoz',
+    author_email='analuzalbornoz@gmail.com',
+    description=('un simple proceso ETL Extract-Transform-Load')
